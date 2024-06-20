31c290e71c8b2ae71ec4a0ae631a459db13c79dc
nar3ndra <realrick9@gmail.com>
2024-06-19 21:46:41 -0400

    week2

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -31,7 +31,7 @@ function wait3(t) {
 
 function calculateTime(t1, t2, t3) {
     const start = Date.now();
-     return wait1(t1).then(()=>{return wait2(t2)}).then(()=>{return wait1(t3)}).then(()=>{
+     return wait1(t1).then(()=>{return wait2(t2)}).then(()=>{return wait3(t3)}).then(()=>{
         return Date.now()-start;
      })
 }
