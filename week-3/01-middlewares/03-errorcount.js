c8b72d4f10f5e924ec275f1d82d4f875c4b69e00
nar3ndra <realrick9@gmail.com>
2024-06-22 23:50:57 -0400

    week3

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -10,9 +10,15 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-app.get('/user', function(req, res) {
-  throw new Error("User not found");
-  res.status(200).json({ name: 'john' });
+
+
+
+app.get('/user', function(req, res,next) {
+  try{throw new Error("User not found");
+  }catch(err){
+    next(err)
+  }
+  // res.status(200).json({ name: 'john' });
 });
 
 app.post('/user', function(req, res) {
@@ -22,5 +28,12 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+const errorHandler = (error,req,res,next)=>{
+  errorCount = errorCount+1;
+  res.status(404).json({});
+}
+app.use(errorHandler);
+
 
 module.exports = app;
--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
-
+const z = require('zod')
 
 /**
  * Generates a JWT for a given username and password.
@@ -13,8 +13,24 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+const userSchema = z.object({
+    username: z.string(),
+    password: z.string().min(6)
+})
+
 function signJwt(username, password) {
-    // Your code here
+    let name = username;
+    let pwd = password;
+    const valid = userSchema.safeParse({
+        username: name,
+        password: pwd
+    })
+    if(!valid){
+        return null;
+    }
+    return jwt.sign({username: name, password: pwd},jwtPassword);
+
 }
 
 /**
